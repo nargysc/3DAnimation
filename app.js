4d7d4ba5e6c23d547b2338e0d6d73cafdda2790d
nargysc <eliasnargiz@gmail.com>
2024-03-10 17:44:03 +0200

    Fixed JS

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const container = document.querySelector(".container");
 const title = document.querySelector(".title");
 const coffee = document.querySelector('.coffee img');
 const purchase = document.querySelector('.purchase button');
-const desc = document.querySelectorAll(".info h3");
+const desc = document.querySelectorAll(".info");
 const grindtype = document.querySelector('.grindtype')
 //moving animation 
 container.addEventListener("mousemove", (e) => {
@@ -16,20 +16,20 @@ container.addEventListener("mouseenter", (e) => {
   card.style.transition = "none";
   //Popout
   title.style.transform = "translateZ(150px)";
-  coffee.style.transform = "translateZ(150px) rotateZ(-45deg)";
+  coffee.style.transform = "translateZ(150px) rotateZ(-360deg)";
   desc.style.transform = "translateZ(125px)";
-  sizes.style.transform = "translateZ(100px)";
+  grindtype.style.transform = "translateZ(100px)";
   purchase.style.transform = "translateZ(75px)";
 });
 //Animate Out
 container.addEventListener("mouseleave", (e) => {
-  card.style.transition = "all 0.5s ease";
+  card.style.transition = "all 0.2s ease";
   card.style.transform = `rotateY(0deg) rotateX(0deg)`;
   //Popback
   title.style.transform = "translateZ(0px)";
   coffee.style.transform = "translateZ(0px) rotateZ(0deg)";
   desc.style.transform = "translateZ(0px)";
-  sizes.style.transform = "translateZ(0px)";
+  grindtype.style.transform = "translateZ(0px)";
   purchase.style.transform = "translateZ(0px)";
 });
   //popout
